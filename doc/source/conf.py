d205aa708e51a26c005973844ce4b36c8c5e591d
Rithman <darth-maul@mail.ru>
2022-12-17 11:25:37 +0500

    lesson_15

---
+++ doc/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+path = os.path.dirname(os.path.abspath('../../launcher.py'))
+sys.path.insert(0, path)
+
+project = 'Async chat'
+copyright = '2022, Aleksey Blagodetelev'
+author = 'Aleksey Blagodetelev'
+release = '0.8'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
